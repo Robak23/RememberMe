37a9692c5ec81074a1b50a4f6cb499a510461651
Robak23 <dawid.robaczynski@gmail.com>
2016-10-13 22:16:47 +0200

    Initial commit

---
+++ Assets/Player/Scripts/AnimationScript/JumpAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class JumpAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    public GameObject playerObject;
+	// Use this for initialization
+	void Start ()
+	{
+	    _animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    PlayerBounce bounceScript = playerObject.GetComponent<PlayerBounce>();
+	    _animator.SetBool("Jump", bounceScript.TriggerAnimation);
+	}
+}
---
+++ Assets/Player/Scripts/PlayerScripts/PlayerBounce.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerBounce : MonoBehaviour
+    {
+        public bool TriggerAnimation;
+        public float JumpTime;
+
+        private bool _inMovement;
+
+        private float _currentJumpTime;
+        private float _tranformProgress;
+
+        private Vector3 _startPosition;
+        private Vector3 _endPosition;
+
+        void Start()
+        {
+            _inMovement = false;
+            TriggerAnimation = false;
+        }
+
+        void Update()
+        {
+            if (!_inMovement)
+            {
+                _inMovement = StartMoving();
+                _startPosition = gameObject.transform.position;
+                _endPosition = GetInputDirection();
+            }
+            if (_inMovement)
+            {
+                _currentJumpTime += Time.deltaTime;
+                _tranformProgress = _currentJumpTime/JumpTime;
+                gameObject.transform.position = Vector3.Lerp(_startPosition, _endPosition, _tranformProgress);
+
+                if (_tranformProgress > 0.8f)
+                {
+                    TriggerAnimation = false;
+                }
+
+                if (gameObject.transform.position == _endPosition)
+                {
+                    _inMovement = false;
+                }
+            }
+        }
+
+        private bool StartMoving()
+        {
+            if (Input.GetButtonDown("up") 
+                || Input.GetButtonDown("down") 
+                || Input.GetButtonDown("left") 
+                || Input.GetButtonDown("right"))
+            {
+                _tranformProgress = 0f;
+                _currentJumpTime = 0f;
+                _inMovement = true;
+                TriggerAnimation = true;
+                return true;
+            }
+            return false;
+        }
+
+        private Vector3 GetInputDirection()
+        {
+            if (Input.GetButtonDown("up"))
+            {
+                return new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+            }
+            if (Input.GetButtonDown("down"))
+            {
+                return new Vector3(transform.position.x, transform.position.y, transform.position.z -1);
+            }
+            if (Input.GetButtonDown("right"))
+            {
+                return new Vector3(transform.position.x +1, transform.position.y, transform.position.z);
+            }
+            if (Input.GetButtonDown("left"))
+            {
+                return new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+            }
+            return new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
